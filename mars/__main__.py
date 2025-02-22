a9ce216ea8e8494140dd49a92d3ec6f2a8852801
Noe Javet <np.javet@gmail.com>
2025-02-22 15:52:33 +0100

    restructure, add icon

--- mars/__main__.py
+++ mars/__main__.py
@@ -1,9 +1,11 @@
 import uvicorn
 
+# project imports
+from . import create_fastapi_app
 
-uvicorn.run('mars:create_app',
+
+uvicorn.run('mars:create_fastapi_app',
             port=8080,
             reload=True,
             factory=True,
             log_level='debug')
-
