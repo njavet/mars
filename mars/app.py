823e458e8771996e541ddbed6e2b2ce47624d9b8
Noe Javet <np.javet@gmail.com>
2025-02-22 15:54:32 +0100

    remove old code

--- mars/app.py
+++ mars/app.py
@@ -6,7 +6,6 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
 # project imports
-from mars import config
 
 
 def create_fastapi_app():
@@ -18,20 +17,5 @@ def create_fastapi_app():
     async def index(request: Request):
         return templates.TemplateResponse('index.html',
                                           {'request': request})
-
-    @app.post('/chat')
-    async def chat(request: Request):
-        data = await request.json()
-        query = data.get('message')
-
-        # Generate responses
-        non_rag_res = app.kai.generate_non_rag_response(query)
-        rag_res = app.kai.generate_rag_response(query)
-        agentic_rag_res = app.kai.generate_agentic_rag_response(query)
-
-        # Return JSON response with the rendered HTML
-        return JSONResponse({'non_rag_response': non_rag_res,
-                             'rag_response': rag_res,
-                             'agentic_rag_response': agentic_rag_res})
 
     return app
