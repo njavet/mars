40c99991156ca44fb78a4641d040c74254c3193f
Noe Javet <np.javet@gmail.com>
2025-04-17 12:13:56 +0200

    new project structure

--- mars/__main__.py
+++ mars/__main__.py
@@ -1,11 +1,3 @@
-import uvicorn
+from .main import run_app
 
-# project imports
-from . import create_fastapi_app
-
-
-uvicorn.run('mars:create_fastapi_app',
-            port=8080,
-            reload=True,
-            factory=True,
-            log_level='debug')
+run_app()
---
+++ mars/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+import uvicorn
+
+# project imports
+from mars.conf import DEFAULT_PORT
+from mars.web import router
+
+
+def create_app():
+    app = FastAPI()
+
+    app.add_middleware(CORSMiddleware,
+                       allow_origins=['http://localhost:5173'],
+                       allow_methods=["*"],
+                       allow_headers=["*"],)
+
+    app.include_router(router)
+
+    return app
+
+
+def run_app():
+    uvicorn.run('bushido.main:create_app',
+                port=DEFAULT_PORT,
+                reload=True,
+                factory=True,
+                log_level='debug')
+
+
+if __name__ == '__main__':
+    run_app()
