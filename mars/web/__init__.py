8fa15ee7331f9ce574e983e5286fb3a9bb02a3f4
Noe Javet <np.javet@gmail.com>
2025-04-22 16:15:59 +0200

    lm fetch api

--- mars/web/__init__.py
+++ mars/web/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 # project imports
+from .lm import router as lm_router
 from .agent import router as agent_router
 
 router = APIRouter()
 router.include_router(agent_router)
+router.include_router(lm_router)
