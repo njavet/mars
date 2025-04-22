e3406bb20b2b27cca8070440b7786c00af231576
Noe Javet <np.javet@gmail.com>
2025-04-22 16:29:41 +0200

    model select

--- mars/web/lm.py
+++ mars/web/lm.py
@@ -1,14 +1,12 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
 
+# project imports
+from mars.conf import LLMS
 
 router = APIRouter()
 
 
 @router.get('/api/lms')
 async def get_lms(request: Request):
-    data = await request.json()
-    query = data.get('query')
-    return JSONResponse({'response': query})
-
-
+    return LLMS
