2a16326ac052cf120082d28b5534fc2bc7c1fc6a
Noe Javet <np.javet@gmail.com>
2025-04-24 17:01:10 +0200

    basic rag

--- mars/schemas.py
+++ mars/schemas.py
@@ -7,21 +7,8 @@ class QueryRequest(BaseModel):
     base_url: str
 
 
-class SentenceSpec(BaseModel):
-    faiss_index: int
-    text: str
-    source: str
-    page_number: int
-
-
 class RagDocument(BaseModel):
     text: str
     source: str
     page_number: int
     distance: float = -1.0
-
-
-class AugmentedQuery(BaseModel):
-    prompt: str
-    rag_docs: list[RagDocument]
-    system_message: str
--- mars/service/rag.py
+++ mars/service/rag.py
@@ -1,23 +1,42 @@
-import faiss
 import numpy as np
 from sentence_transformers import SentenceTransformer
-from typing import List
+
+# project imports
+from mars.conf import SENTENCE_TRANSFORMER_NAME
+from mars.utils.text_helpers import extract_pdfs
+from mars.schemas import RagDocument
+from mars.data.repo import Repository
 
 
 class RAG:
-    def __init__(self, model_name="all-MiniLM-L6-v2"):
-        self.embedder = SentenceTransformer(model_name)
-        self.index = None
-        self.documents = []
+    def __init__(self, repo: Repository) -> None:
+        self.model = SentenceTransformer(SENTENCE_TRANSFORMER_NAME)
+        self.repo = repo
 
-    def build_index(self, texts: List[str]):
-        self.documents = texts
-        embeddings = self.embedder.encode(texts, convert_to_numpy=True)
-        dim = embeddings.shape[1]
-        self.index = faiss.IndexFlatL2(dim)
-        self.index.add(embeddings)
+    def embed_documents(self):
+        sentences = extract_pdfs()
+        for idx, sentence in enumerate(sentences):
+            embedding = self.model.encode(sentence.text)
+            self.repo.add_embedding(embedding)
+            sentence.faiss_index = idx
+        self.repo.save_faiss_index()
+        self.repo.save_sentences(sentences)
 
-    def retrieve(self, query: str, top_k: int = 3) -> List[str]:
-        query_embedding = self.embedder.encode([query], convert_to_numpy=True)
-        distances, indices = self.index.search(query_embedding, top_k)
-        return [self.documents[i] for i in indices[0]]
+    def retrieve_documents(self,
+                           query: str,
+                           k: int = 5,
+                           threshold: float = 2.0) -> list[RagDocument]:
+        query_embedding = self.model.encode(query, convert_to_numpy=True)
+        qa = np.array([query_embedding])
+        distances, indices = self.repo.search_index(qa, k)
+        documents = []
+        for i, idx in enumerate(indices[0]):
+            if idx != -1:
+                if distances[0][i] < threshold:
+                    sentence = self.repo.get_sentence(idx)
+                    rag_doc = RagDocument(text=sentence.text,
+                                          source=sentence.source,
+                                          page_number=sentence.page_number,
+                                          distance=distances[0][i])
+                    documents.append(rag_doc)
+        return documents
