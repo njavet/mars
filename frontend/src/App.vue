9f43ad4c52eabb3cca3a2a7f4032d57c3c9dff8e
Noe Javet <np.javet@gmail.com>
2025-04-24 14:18:32 +0200

    adjust frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="app-container">
-    <Sidebar @model-selected="selectedModel = $event"/>
+    <Sidebar
+        @model-selected="selectedModel = $event"
+        @baseUrl="selectedServer = $event"
+    />
     <div class="main-content" v-if="selectedModel">
-      <Chat :model="selectedModel" />
+      <Chat :lm_name="selectedModel" :base_url="selectedServer" />
     </div>
   </div>
 </template>
 
 <script setup>
-import {ref} from "vue";
+import { ref } from "vue";
 import Sidebar from './components/Sidebar.vue'
 import Chat from './components/Chat.vue'
 const selectedModel = ref("")
+const selectedServer = ref("")
 
 </script>
 
