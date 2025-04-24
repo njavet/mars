0ae586f78848bf3f34fe6ede7109de67dc251586
Noe Javet <np.javet@gmail.com>
2025-04-24 17:01:22 +0200

    data access repo

--- mars/data/repo.py
+++ mars/data/repo.py
@@ -1,4 +1,5 @@
 from sqlalchemy import select
+import faiss
 
 # project imports
 from mars.schemas import RagDocument
@@ -8,17 +9,23 @@ from mars.data.tables import Sentence
 class Repository:
     def __init__(self, session):
         self.session = session
+        self.index = faiss.read_index('index.faiss')
+
+    def add_embedding(self, embedding):
+        self.index.add(embedding.reshape(1, -1))
+
+    def save_faiss_index(self):
+        faiss.write_index(self.index, 'index.faiss')
 
     def save_sentences(self, sentences: list[Sentence]):
         self.session.add_all(sentences)
         self.session.commit()
 
-    def get_sentences(self):
-        stmt = select(Sentence)
-        results = self.session.scalars(stmt).all()
-        dix = {}
-        for result in results:
-            dix[result.faiss_index] = RagDocument(text=result.text,
-                                                  source=result.source,
-                                                  page_number=result.page_number)
-        return dix
+    def search_index(self, query_array, k):
+        distances, indices = self.index.search(query_array, k)
+        return distances, indices
+
+    def get_sentence(self, faiss_index):
+        stmt = select(Sentence).where(Sentence.faiss_index == faiss_index)
+        result = self.session.scalars(stmt).one()
+        return result
