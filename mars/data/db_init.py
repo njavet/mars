79fb246fff90a080dbf3222b8cb25006a46f25f3
Noe Javet <np.javet@gmail.com>
2025-04-24 17:42:03 +0200

    db init

--- mars/data/db_init.py
+++ mars/data/db_init.py
@@ -1,10 +1,17 @@
 from sqlalchemy import create_engine
+import faiss
+from sentence_transformers import SentenceTransformer
 
 # project imports
+from mars.conf import DB_URL, SENTENCE_TRANSFORMER_NAME
 from mars.data.tables import Base
 
 
 def db_init():
-    engine = create_engine('sqlite:///sentence.db')
+    engine = create_engine(DB_URL)
     Base.metadata.create_all(engine)
-    return engine
+
+    model = SentenceTransformer(SENTENCE_TRANSFORMER_NAME)
+    dimension = model.get_sentence_embedding_dimension()
+    index = faiss.IndexFlatL2(dimension)
+    faiss.write_index(index, 'index.faiss')
