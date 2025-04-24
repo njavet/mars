02f5c744318e7ca7b8bd5f4d1cb46d829194f58e
Noe Javet <np.javet@gmail.com>
2025-04-24 13:52:24 +0200

    api

--- mars/service/agent.py
+++ mars/service/agent.py
@@ -8,3 +8,9 @@ class Agent:
 
     def run_query(self, query: str) -> str:
         return self.lm.generate(query)
+
+
+def get_agent(lm_name, base_url):
+    lm = LanguageModel(name=lm_name, base_url=base_url)
+    agent = Agent(lm)
+    return agent
--- mars/web/endpoints.py
+++ mars/web/endpoints.py
@@ -1,10 +1,12 @@
+import io
 from fastapi import APIRouter, Request, UploadFile, File, Query
 from fastapi.responses import JSONResponse
-import io
 from docx import Document
 
 # project imports
 from mars.conf import LMS
+from mars.schemas.req import QueryRequest
+from mars.service.agent import get_agent
 
 
 router = APIRouter()
@@ -16,19 +18,17 @@ async def get_lms(request: Request):
 
 
 @router.post('/api/chat')
-async def chat(request: Request, lm_name: str = Query(...)) -> JSONResponse:
-    data = await request.json()
-    query = data.get('query')
-    service = LMAgentService(lm_name, 'http://localhost:11434')
-    return JSONResponse({'response': service.handle_query(query)})
+async def chat(payload: QueryRequest) -> JSONResponse:
+    agent = get_agent(payload.lm_name, payload.base_url)
+    return JSONResponse({'response': agent.run_query(payload.query)})
 
 
 @router.post('/api/upload-docx')
 async def upload_docx(file: UploadFile = File(...),
-                      lm_name: str = Query(...)) -> JSONResponse:
+                      lm_name: str = Query(...),
+                      base_url: str = Query(...)) -> JSONResponse:
     contents = await file.read()
     doc = Document(io.BytesIO(contents))
+    agent = get_agent(lm_name, base_url)
     text = '\n'.join([para.text for para in doc.paragraphs])
-    service = LMAgentService(lm_name, 'http://localhost:11434')
-    return JSONResponse({'response': service.handle_query(text)})
-
+    return JSONResponse({'response': agent.run_query(text)})
