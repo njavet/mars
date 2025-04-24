76ef693ee8b09b8c84420dcf946104412afca704
Noe Javet <np.javet@gmail.com>
2025-04-24 14:24:57 +0200

    frontend fix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
   <div class="app-container">
     <Sidebar
         @model-selected="selectedModel = $event"
-        @baseUrl="selectedServer = $event"
+        @server-selected="selectedServer = $event"
     />
     <div class="main-content" v-if="selectedModel">
       <Chat :lm_name="selectedModel" :base_url="selectedServer" />
@@ -15,7 +15,7 @@ import { ref } from "vue";
 import Sidebar from './components/Sidebar.vue'
 import Chat from './components/Chat.vue'
 const selectedModel = ref("")
-const selectedServer = ref("")
+const selectedServer = ref("http://localhost:11434")
 
 </script>
 
