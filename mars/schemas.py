65ebc6dce3808219d29753a3f387cb8d1ed48e65
Noe Javet <np.javet@gmail.com>
2025-04-24 16:04:13 +0200

    new schemas

--- mars/schemas.py
+++ mars/schemas.py
@@ -6,3 +6,22 @@ class QueryRequest(BaseModel):
     lm_name: str
     base_url: str
 
+
+class SentenceSpec(BaseModel):
+    faiss_index: int
+    text: str
+    source: str
+    page_number: int
+
+
+class RagDocument(BaseModel):
+    text: str
+    source: str
+    page_number: int
+    distance: float = -1.0
+
+
+class AugmentedQuery(BaseModel):
+    prompt: str
+    rag_docs: list[RagDocument]
+    system_message: str
