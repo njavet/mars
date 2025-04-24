fcff91262125bee42342e1831364a586b68b0604
Noe Javet <np.javet@gmail.com>
2025-04-24 17:24:47 +0200

    fastapi deps

--- mars/web/endpoints.py
+++ mars/web/endpoints.py
@@ -1,12 +1,14 @@
 import io
-from fastapi import APIRouter, Request, UploadFile, File, Query
+from fastapi import APIRouter, Request, UploadFile, File, Query, Depends
 from fastapi.responses import JSONResponse
 from docx import Document
+from sqlalchemy.orm import Session
 
 # project imports
 from mars.conf import LMS
 from mars.schemas import QueryRequest
 from mars.service.agent import get_agent
+from mars.web.deps import get_db
 
 
 router = APIRouter()
@@ -18,7 +20,7 @@ async def get_lms(request: Request):
 
 
 @router.post('/api/chat')
-async def chat(payload: QueryRequest) -> JSONResponse:
+async def chat(payload: QueryRequest, session: Session = Depends(get_db)) -> JSONResponse:
     agent = get_agent(payload.lm_name, payload.base_url)
     return JSONResponse({'response': agent.run_query(payload.query)})
 
@@ -26,7 +28,8 @@ async def chat(payload: QueryRequest) -> JSONResponse:
 @router.post('/api/upload-docx')
 async def upload_docx(file: UploadFile = File(...),
                       lm_name: str = Query(...),
-                      base_url: str = Query(...)) -> JSONResponse:
+                      base_url: str = Query(...),
+                      session: Session = Depends(get_db)) -> JSONResponse:
     contents = await file.read()
     doc = Document(io.BytesIO(contents))
     agent = get_agent(lm_name, base_url)
