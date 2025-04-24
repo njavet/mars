48db933568d23e34f9b72ffcc86d84926e76a11b
Noe Javet <np.javet@gmail.com>
2025-04-24 18:26:01 +0200

    agent prompt

--- mars/utils/prompt.py
+++ mars/utils/prompt.py
@@ -8,10 +8,5 @@ def get_prompt(fname=PROMPT_PATH):
     with open(fname, 'rb') as f:
         config = tomli.load(f)
 
-    prompt = config["prompts"]["evaluation"]
+    prompt = config['prompts']['evaluation']
     return prompt
-
-
-def get_full_prompt(prompt, doc_text):
-    full_prompt = prompt.format(doc_text=doc_text)
-    return full_prompt
