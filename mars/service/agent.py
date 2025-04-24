a6f0ace08c7d518069a6ef7902a3934138f55ee4
Noe Javet <np.javet@gmail.com>
2025-04-24 18:26:01 +0200

    agent prompt

--- mars/service/agent.py
+++ mars/service/agent.py
@@ -1,8 +1,8 @@
 # project imports
+from mars.utils.prompt import get_prompt
 from mars.data.repo import Repository
 from mars.service.lm import LanguageModel
 from mars.service.rag import RAG
-
 
 
 class Agent:
@@ -11,7 +11,9 @@ class Agent:
         self.rag = rag
 
     def run_query(self, query: str) -> str:
-        return self.lm.generate(query)
+        prompt = get_prompt()
+        full_prompt = prompt.format(doc_text=query)
+        return self.lm.generate(full_prompt)
 
 
 def get_agent(lm_name, base_url, session):
