96426f5528548a35dbc2aa46532f40400a463309
Noe Javet <np.javet@gmail.com>
2025-04-24 16:22:05 +0200

    base rag refactoring

--- mars/service/rag/base_rag.py
+++ mars/service/rag/base_rag.py
@@ -1,22 +1,16 @@
 from abc import ABC
 from sentence_transformers import SentenceTransformer
-from pathlib import Path
 
 # project imports
-from mars.service.rag.base_embedder import BaseEmbedder
+from mars.conf import SENTENCE_TRANSFORMER_NAME
+from mars.data.repo import Repository
 
 
 class BaseRAG(ABC):
-    def __init__(self,
-                 sentence_transformer_name: str,
-                 doc_folder: Path) -> None:
-        self.sentence_transformer_name = sentence_transformer_name
-        self.model = self.create_sentence_transformer()
-        self.embedder = BaseEmbedder(self.model,
-                                     doc_folder)
-
-    def create_sentence_transformer(self):
-        return SentenceTransformer(self.sentence_transformer_name)
+    def __init__(self, repo: Repository) -> None:
+        self.repo = repo
+        self.model = SentenceTransformer(SENTENCE_TRANSFORMER_NAME)
+        self.embedder = None
 
     def retrieve_documents(self, query: str, k: int):
         raise NotImplementedError
