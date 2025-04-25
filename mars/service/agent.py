3b87665927052ac2dc535d4f2bec4e7dc18378c9
Noe Javet <np.javet@gmail.com>
2025-04-25 16:22:02 +0200

    adjust prompts

--- mars/service/agent.py
+++ mars/service/agent.py
@@ -9,30 +9,34 @@ class Agent:
         self.lm = lm
         self.rag = None
 
-    def run_query(self, query: str, preprompt: str) -> str:
-        if not preprompt:
-            preprompt = f'{query}'
-            full_prompt = query
+    def run_query(self,
+                  enable_rag: bool,
+                  preprompt: str,
+                  query: str) -> str:
 
-        if self.rag and preprompt:
+        if preprompt:
+            if enable_rag:
+                docs = self.rag.retrieve_documents(query)
+                full_prompt = preprompt.format(doc_text=docs,
+                                               query=query)
+
+        if enable_rag and not preprompt:
             docs = self.rag.retrieve_documents(query)
-            full_prompt = preprompt.format(doc_text=docs,
-                                           query=query)
+            full_prompt = '\n'.join([docs, query])
+        elif enable_rag:
         else:
             full_prompt = preprompt.format(doc_text=query)
         res = self.lm.generate(full_prompt)
-        print('res', res)
         return res
 
     def set_rag(self, rag: RAG):
         self.rag = rag
 
 
-def get_agent(base_url, lm_name, enable_rag, session):
+def get_agent(base_url, lm_name, session):
     lm = LanguageModel(name=lm_name, base_url=base_url)
     agent = Agent(lm)
-    if enable_rag:
-        repo = Repository(session=session)
-        rag = RAG(repo)
-        agent.set_rag(rag)
+    repo = Repository(session=session)
+    rag = RAG(repo)
+    agent.set_rag(rag)
     return agent
