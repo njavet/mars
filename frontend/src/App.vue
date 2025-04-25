c7ad36c09d74ededa6c5ee96205bc55b3c37e83f
Noe Javet <np.javet@gmail.com>
2025-04-25 15:25:12 +0200

    frontend settings

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,12 +2,11 @@
   <div class="app-container">
     <Sidebar
         :selectedView="selectedView"
-        @server-selected="selectedServer = $event"
         @view-selected="selectedView = $event"
     />
     <div class="main-content">
       <Home v-if="selectedView === 'home'"/>
-      <Chatbot v-if="selectedView === 'chatbot'" :base_url="selectedServer" />
+      <Chatbot v-if="selectedView === 'chatbot'"" />
       <Assistant v-else-if="selectedView === 'assistant'"/>
     </div>
   </div>
@@ -19,7 +18,6 @@ import Sidebar from './components/Sidebar.vue'
 import Home from './components/Home.vue'
 import Chatbot from './components/Chatbot.vue'
 import Assistant from "./components/Assistant.vue";
-const selectedServer = ref("http://localhost:11434")
 const selectedView = ref('home')
 </script>
 
