6627b3f4b16394c4f201fe94a6bb50273d3c8282
Noe Javet <np.javet@gmail.com>
2025-04-25 14:24:26 +0200

    prepromt endpoint

--- mars/utils/prompt.py
+++ mars/utils/prompt.py
@@ -4,9 +4,7 @@ import tomli
 from mars.conf import PROMPT_PATH
 
 
-def get_prompt(prompt_mode: str, fname=PROMPT_PATH):
+def load_preprompts(fname=PROMPT_PATH):
     with open(fname, 'rb') as f:
-        config = tomli.load(f)
-
-    prompt = config['prompts'][prompt_mode]
-    return prompt
+        data = tomli.load(f)
+    return data.get('preprompts', [])
