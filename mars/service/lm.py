fb228645ebdb9d419233b36ae75b57a16830d093
Noe Javet <np.javet@gmail.com>
2025-04-26 20:22:20 +0200

    lm temperature

--- mars/service/lm.py
+++ mars/service/lm.py
@@ -3,15 +3,32 @@ import requests
 
 
 class LanguageModel:
-    def __init__(self, name: str, base_url: str) -> None:
+    def __init__(self,
+                 name: str,
+                 base_url: str,
+                 temperature: float = 0.5) -> None:
         self.name = name
         self.base_url = base_url
+        self.temperature = temperature
+
+    @property
+    def temperature(self) -> float:
+        return self._temperature
+
+    @temperature.setter
+    def temperature(self, temperature: float) -> None:
+        if temperature < 0:
+            raise ValueError('temperature cannot be negative')
+        self._temperature = temperature
 
     def generate(self, prompt: str) -> str:
         logger.info(f'[LM] generate: {self.base_url}')
         res = requests.post(
             url=f'{self.base_url}/api/generate',
-            json={'model': self.name, 'prompt': prompt, 'stream': False}
+            json={'model': self.name,
+                  'prompt': prompt,
+                  'temperature': self.temperature,
+                  'stream': False}
         )
         res.raise_for_status()
         return res.json()['response']
