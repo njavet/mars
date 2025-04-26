0ca8aea43e5c09b3f5f2f9f4fb9a523aedbb92ed
Noe Javet <np.javet@gmail.com>
2025-04-26 17:09:45 +0200

    embedding service

--- mars/data/db_init.py
+++ mars/data/db_init.py
@@ -1,13 +1,7 @@
-import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-import faiss
-from sentence_transformers import SentenceTransformer
 
 # project imports
-from mars.conf import DB_URL, SENTENCE_TRANSFORMER_NAME
-from mars.utils.text_helpers import extract_pdfs
-from mars.data.repo import Repository
+from mars.conf import DB_URL
 from mars.data.tables import Base
 
 
@@ -15,26 +9,3 @@ def db_init():
     # init sqlalchemy
     engine = create_engine(DB_URL)
     Base.metadata.create_all(engine)
-
-    if not os.path.exists('index.faiss'):
-        print('creating faiss index...')
-        # create faiss index
-        model = SentenceTransformer(SENTENCE_TRANSFORMER_NAME)
-        dimension = model.get_sentence_embedding_dimension()
-        index = faiss.IndexFlatL2(dimension)
-        faiss.write_index(index, 'index.faiss')
-
-        # embed pdfs
-        with Session(engine) as session:
-            repo = Repository(session)
-            embed_documents(model, repo)
-
-
-def embed_documents(model, repo):
-    sentences = extract_pdfs()
-    for idx, sentence in enumerate(sentences):
-        embedding = model.encode(sentence.text)
-        repo.add_embedding(embedding)
-        sentence.faiss_index = idx
-    repo.save_faiss_index()
-    repo.save_sentences(sentences)
---
+++ mars/service/embedder.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+from rich.console import Console
+import numpy as np
+import pdfplumber
+from sentence_transformers import SentenceTransformer
+import faiss
+import tiktoken
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# project imports
+from mars.conf import PDF_DIR, CHUNK_SIZE, OVERLAP, SENTENCE_TRANSFORMER_NAME
+from mars.data.tables import Sentence
+
+
+class EmbeddingService:
+    def __init__(self, sql_repo):
+        self.console = Console()
+        self.model = SentenceTransformer(SENTENCE_TRANSFORMER_NAME)
+        self.sql_repo = sql_repo
+
+    def encode(self, texts: list[str]) -> np.ndarray:
+        return self.model.encode(texts, convert_to_numpy=True)
+
+    def embed_documents(self):
+        new_docs = self.sql_repo.get_new_documents()
+        sentences = []
+        for pdf_path in PDF_DIR.glob('*.pdf'):
+            if pdf_path.name in new_docs:
+                try:
+                    new_sentences = self.extract_pages_with_metadata(pdf_path)
+                except Exception as e:
+                    self.console.print(f'exception for {pdf_path}', e)
+                else:
+                    sentences.extend(new_sentences)
+        vectors = self.encode([sentence.text for sentence in sentences])
+        self.sql_repo.add_bulk(sentences, vectors)
+
+    def extract_pages_with_metadata(self, pdf_path: Path) -> list[Sentence]:
+        sentences = []
+        with pdfplumber.open(pdf_path) as pdf:
+            for i, page in enumerate(pdf.pages, start=1):
+                text = page.extract_text()
+                if not text:
+                    continue
+                cleaned_text = self.text_cleaning(text)
+                for chunk in self.split_text(cleaned_text):
+                    sentence = Sentence(text=chunk,
+                                        source=pdf_path.name,
+                                        page_number=i)
+                    sentences.append(sentence)
+        return sentences
+
+    def split_text(self, text: str,
+                   chunk_size: int = CHUNK_SIZE,
+                   overlap: int = OVERLAP) -> list[str]:
+        words = text.split()
+        chunks = []
+        start = 0
+        while start < len(words):
+            end = start + chunk_size
+            chunk = ' '.join(words[start:end])
+            chunks.append(chunk)
+            start += chunk_size - overlap
+        return chunks
+
+    def text_cleaning(self, text: str) -> str:
+        """ remove newlines, too many whitespaces """
+        # remove header and footer
+        t0 = text.replace('\n', ' ')
+        t1 = ' '.join(t0.split())
+        return t1
