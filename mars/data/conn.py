68affa2baa69ce79dd0aeecca1403ee3678fe6e4
Noe Javet <np.javet@gmail.com>
2025-05-03 16:26:25 +0200

    fastapi DI

--- mars/data/conn.py
+++ mars/data/conn.py
@@ -1,4 +1,3 @@
-from contextlib import contextmanager
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -9,15 +8,18 @@ from mars.data.tables import Base
 
 class SessionFactory:
     def __init__(self, db_url=DB_URL):
+        print('I got created...')
         self._engine = create_engine(db_url)
         Base.metadata.create_all(self._engine)
         self._sessionmaker = sessionmaker(bind=self._engine,
                                           expire_on_commit=False)
 
-    @contextmanager
     def get_session(self):
         db = self._sessionmaker()
         try:
             yield db
         finally:
             db.close()
+
+
+session_factory = SessionFactory()
