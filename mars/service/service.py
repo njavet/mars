b4ddaa8b803b7eacce08707bc15f7ab49c2e4b48
Noe Javet <np.javet@gmail.com>
2025-05-03 18:51:53 +0200

    baseline is not an agent

--- mars/service/service.py
+++ mars/service/service.py
@@ -1,3 +1,4 @@
+from fastapi.logger import logger
 from sentence_transformers import SentenceTransformer
 
 # project imports
@@ -5,6 +6,7 @@ from mars.conf import SENTENCE_TRANSFORMER_NAME
 from mars.data.conn import SessionFactory
 from mars.data.faiss_repo import FaissRepository
 from mars.data.sql_repo import SqlRepository
+from mars.service.lm import LanguageModel
 from mars.service.rag import RAG
 
 
@@ -17,3 +19,22 @@ def init_rag():
                              faiss_repo=faiss_repo)
     rag = RAG(st_model, sql_repo)
     return rag
+
+
+def run_query(base_url: str,
+              lm_name: str,
+              system_message: str,
+              query: str,
+              rag: RAG = None) -> str:
+    lm = LanguageModel(name=lm_name, base_url=base_url)
+    logger.info(f'[Baseline] Running query with {lm.name}')
+
+    if rag:
+        docs = rag.retrieve_documents(query)
+        logger.info(f'[Baseline] Retrieved docs...')
+        doc_msg = '\n'.join([rag_doc.text for rag_doc in docs])
+        system_message = '\n'.join([system_message, doc_msg])
+
+    res = lm.chat(system_message=system_message, query=query)
+    logger.debug(f'[Baseline] LLM response generated: {res}')
+    return res
