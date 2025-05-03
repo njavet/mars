9e84efea853468265b49d28abc519b936e23c2d5
Noe Javet <np.javet@gmail.com>
2025-05-03 18:46:08 +0200

    rag refactoring

--- mars/web/deps.py
+++ mars/web/deps.py
@@ -1,25 +1,10 @@
-from sentence_transformers import SentenceTransformer
-
 # project imports
-from mars.conf import SENTENCE_TRANSFORMER_NAME
-from mars.data.conn import SessionFactory
-from mars.data.faiss_repo import FaissRepository
+from mars.service.service import init_rag
 
 
 # singleton global instances
-session_factory = SessionFactory()
-st_model = SentenceTransformer(SENTENCE_TRANSFORMER_NAME)
-faiss_repo = FaissRepository(dim=st_model.get_sentence_embedding_dimension())
+rag = init_rag()
 
 
-def get_session():
-    return session_factory.get_session()
-
-
-def get_st_model():
-    return st_model
-
-
-def get_faiss_repo():
-    return faiss_repo
-
+def get_rag():
+    return rag
