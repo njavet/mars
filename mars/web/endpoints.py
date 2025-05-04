87ace93363c7e4c9925f5f1c92344e6fae141c05
Noe Javet <np.javet@gmail.com>
2025-05-04 15:38:40 +0200

    wip

--- mars/web/endpoints.py
+++ mars/web/endpoints.py
@@ -13,7 +13,8 @@ from mars.schemas import QueryRequest
 from mars.utils.prompt import load_prompts
 from mars.utils.text_formatters import format_as_markdown
 from mars.service.rag_context import app_context
-from mars.service.service import run_query
+from mars.service.service import (run_baseline,
+                                  run_baseline_rag)
 
 
 router = APIRouter()
@@ -35,44 +36,51 @@ async def get_system_messages() -> JSONResponse:
 
 @router.post('/api/baseline/base')
 async def baseline(payload: QueryRequest) -> JSONResponse:
-    res = run_query(base_url=payload.base_url,
-                    lm_name=payload.lm_name,
-                    system_message=payload.system_message,
-                    query=payload.query)
+    res = run_baseline(base_url=payload.base_url,
+                       lm_name=payload.lm_name,
+                       system_message=payload.system_message,
+                       query=payload.query)
     return JSONResponse({'response': format_as_markdown(res)})
 
 
 @router.post('/api/baseline/rag')
 async def baseline_rag(payload: QueryRequest) -> JSONResponse:
     rag = app_context.rag
-    res = run_query(base_url=payload.base_url,
-                    lm_name=payload.lm_name,
-                    system_message=payload.system_message,
-                    query=payload.query,
-                    rag=rag)
+    res = run_baseline_rag(base_url=payload.base_url,
+                           lm_name=payload.lm_name,
+                           system_message=payload.system_message,
+                           query=payload.query,
+                           rag=rag)
     return JSONResponse({'response': format_as_markdown(res)})
 
 
-@router.post('/api/baseline/upload-docx')
+@router.post('/api/baseline/base-docx')
 async def upload_docx(file: UploadFile = File(...),
                       base_url: str = Form(...),
                       lm_name: str = Form(...),
-                      enable_rag: bool = Form(...),
                       system_message: str = Form(...)) -> JSONResponse:
     contents = await file.read()
     doc = Document(io.BytesIO(contents))
     text = '\n'.join([para.text for para in doc.paragraphs])
+    res = run_baseline(base_url=base_url,
+                       lm_name=lm_name,
+                       system_message=system_message,
+                       query=text)
+    return JSONResponse({'response': res})
 
-    if enable_rag:
-        rag = app_context.rag
-        res = run_query(base_url=base_url,
-                        lm_name=lm_name,
-                        system_message=system_message,
-                        query=text,
-                        rag=rag)
-    else:
-        res = run_query(base_url=base_url,
-                        lm_name=lm_name,
-                        system_message=system_message,
-                        query=text)
+
+@router.post('/api/baseline/rag-docx')
+async def upload_docx(file: UploadFile = File(...),
+                      base_url: str = Form(...),
+                      lm_name: str = Form(...),
+                      system_message: str = Form(...)) -> JSONResponse:
+    contents = await file.read()
+    doc = Document(io.BytesIO(contents))
+    text = '\n'.join([para.text for para in doc.paragraphs])
+    rag = app_context.rag
+    res = run_baseline_rag(base_url=base_url,
+                           lm_name=lm_name,
+                           system_message=system_message,
+                           query=text,
+                           rag=rag)
     return JSONResponse({'response': res})
