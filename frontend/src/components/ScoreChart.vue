b69e7855486993220823fe6fd43253fbebf8582e
Noe Javet <np.javet@gmail.com>
2025-05-06 13:24:28 +0200

    score chart

---
+++ frontend/src/components/ScoreChart.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { Bar } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+const props = defineProps({
+  scores: Object
+})
+
+const labels = Object.keys(props.scores)
+const data = {
+  labels,
+  datasets: [{
+    label: 'Evaluation',
+    data: labels.map(k => props.scores[k]),
+    backgroundColor: 'cyan'
+  }]
+}
+</script>
+
+<template>
+  <Bar :data="data" />
+</template>
