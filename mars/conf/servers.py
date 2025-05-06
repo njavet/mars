83acf92770ef47509e60ac014221430fb942d6f3
Noe Javet <np.javet@gmail.com>
2025-05-06 18:31:46 +0200

    servers to backend

---
+++ mars/conf/servers.py
@@ -0,0 +1,20 @@
+base_servers = [
+    'http://localhost',
+    'http://sandiego.zhaw.ch',
+    'http://sanfrancisco.zhaw.ch',
+    'http://losangeles.zhaw.ch',
+    'http://sacramento.zhaw.ch',
+    'http://sanjose.zhaw.ch',
+    'http://fresko.zhaw.ch',
+    'http://trinity.zhaw.ch',
+    'http://elpaso.zhaw.ch',
+    'http://lubbock.zhaw.ch',
+    'http://honolulu.zhaw.ch',
+    'http://hilo.zhaw.ch'
+]
+
+ports = {
+    'default': 11434,
+    'tosh': 8800,
+    'ahmed': 8600
+}
