33ad1e7ec456081e781ea1de21e6ec17be41f98a
Noe Javet <np.javet@gmail.com>
2025-05-06 18:58:04 +0200

    wip

--- mars/web/fetch_config.py
+++ mars/web/fetch_config.py
@@ -13,10 +13,10 @@ router = APIRouter()
 
 @router.get('/api/servers')
 async def fetch_servers():
-    res = subprocess.run(['whoami'], capture_output=True, text=True)
-    username = res.stdout.strip()
+    result = subprocess.run(['whoami'], capture_output=True, text=True)
+    username = result.stdout.strip()
     port = PORTS.get(username, 11434)
-    servers = [':'.join([server, port]) for server in SERVERS]
+    servers = [':'.join([server, str(port)]) for server in SERVERS]
     return JSONResponse(content={'servers': servers})
 
 
