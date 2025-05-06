5fef6b69a0e74acbd0b63f514454ba79a9215e65
Noe Javet <np.javet@gmail.com>
2025-05-06 16:23:40 +0200

    interface

--- mars/web/__init__.py
+++ mars/web/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 # project imports
 from .fetch_config import router as fetch_config_router
+from .chat import router as chat_router
 from .endpoints import router as endpoints_router
 
 
 router = APIRouter()
 router.include_router(fetch_config_router)
+router.include_router(chat_router)
 router.include_router(endpoints_router)
