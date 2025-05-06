5f1c3e1ad5fd719f69bb842d304e33cbeda708f7
Noe Javet <np.javet@gmail.com>
2025-05-06 19:33:26 +0200

    fix

--- mars/utils/helpers.py
+++ mars/utils/helpers.py
@@ -21,7 +21,7 @@ def format_as_markdown(text: str) -> str:
 
 def read_docx(docx_path: Path):
     doc = Document(docx_path)
-    text = '\n'.join([para.text for para in doc.paragraphs])
+    text = ''.join([para.text for para in doc.paragraphs])
     return text
 
 
