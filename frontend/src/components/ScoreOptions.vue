516f01c51617b0b447b8f454060c7b1b42a4c2ef
Noe Javet <np.javet@gmail.com>
2025-05-07 19:01:29 +0200

    score options

---
+++ frontend/src/components/ScoreOptions.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="binary-matrix">
+<div v-for="(label, key) in ['complete', 'irrelevant', 'concise']" :key="key">
+  <span>{{ label }}</span>
+  <label>
+    <input
+      type="radio"
+      :name="label"
+      value="yes"
+      v-model="scores[label]"
+    /> Yes
+  </label>
+  <label>
+    <input
+      type="radio"
+      :name="label"
+      value="no"
+      v-model="scores[label]"
+    /> No
+  </label>
+</div>
+
+
+  </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+const scores = defineModel('scores')
+// TODO fetch from backend
+const categories = ['complete', 'irrelevant', 'concise']
+const selections = reactive({
+  complete: null,
+  irrelevant: null,
+  concise: null
+})
+</script>
+
+<style scoped>
+.binary-matrix {
+  display: flex;
+  border: 2px solid #6312ff;
+  flex-direction: column;
+  padding: 1rem;
+  gap: 0.5rem;
+}
+.binary-row {
+  display: flex;
+  align-items: center;
+  text-align: left;
+  gap: 1rem;
+}
+.label {
+  width: 100px;
+  font-weight: bold;
+}
+</style>
