90811328e46667aff5411f89364a7cec14355443
Noe Javet <np.javet@gmail.com>
2025-05-07 19:59:47 +0200

    eval

--- mars/web/eval.py
+++ mars/web/eval.py
@@ -1,10 +1,10 @@
 import json
 import os
-from fastapi import APIRouter, Request, HTTPException
+from fastapi import APIRouter, HTTPException
 
 # project imports
 from mars.conf.conf import RESULTS_DIR
-from mars.schemas import Evaluation
+from mars.schemas import Evaluation, ScoreEntry
 from mars.utils.helpers import get_number_of_runs
 
 
@@ -32,27 +32,8 @@ def fetch_eval_results(run: int) -> list[Evaluation]:
 
 
 @router.post("/api/save-scores")
-async def save_scores(req: Request):
-    data = await req.json()
-    file = data['file']
-    lm_name = data['lm_name']
-    scores = data['scores']
-
-    path = f"frontend/public/results/{file}"
-    print(f"saving scores to {path}")
-    if not os.path.exists(path):
-        return {"error": "File not found"}
-
-    with open(path) as f:
-        entries = json.load(f)
-
+async def save_scores(entries: list[ScoreEntry]):
     for entry in entries:
-        if entry["lm_name"] == lm_name:
-            entry["scores"] = scores
-            break
-
-    with open(path, "w") as f:
-        json.dump(entries, f, indent=2)
+        print('storing', entry)
 
     return {"status": "ok"}
-
