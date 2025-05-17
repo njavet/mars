ea696782bd2188d05ece58a70939db01e0d4a51b
Noe Javet <np.javet@gmail.com>
2025-05-17 17:14:52 +0200

    runtime 4096 token max vs model capability of 32768 tokens

--- mars/prompts/medical.py
+++ mars/prompts/medical.py
@@ -1,8 +1,3 @@
 medical_assistant_0 = """
-You are a medical assistant for psychiatrists. You receive a discharge report
-and your job is to check if important medical information is missing.
-
-do NOT summarize the report!
-answer CONCISE and in german!
-at the end, repeat the last line of the report!
+output the received user message. Nothing else!
 """
