62ce0b20b2b5728b7a751722eb511cfac32c14d0
Noe Javet <np.javet@gmail.com>
2025-05-17 18:22:52 +0200

    screenshot for current medical system message

--- mars/prompts/medical.py
+++ mars/prompts/medical.py
@@ -1,18 +1,20 @@
 medical_assistant_0 = """
 You are an assistant for psychiatrists and your style of communication is 
-VERY concise and formal. for every section which is between '<' and '>' 
+VERY concise and formal. 
+
+you will receive a report which is divided in sections with tags: 
+for every such section which is between '<' and '>' 
 you decide if information is missing. when you think there is something
 missing, you reply with ONLY '0'.
-if you think it is complete, you answer with ONLY '1'.
+if you think it is complete, you answer with ONLY '1'. IMPORTANT: do NOT 
+say anything else!
 
 example:
-
 input:
 <diagnose>
 keine
 <gewicht>
 90kg
-
 your response:
 <diagnose> 0
 <gewicht> 1
--- mars/utils/helpers.py
+++ mars/utils/helpers.py
@@ -7,15 +7,15 @@ from mars.schemas import SystemMessage
 
 def load_system_messages():
     lst = []
-    for name in dir(prompts):
-        if not name.startswith('_'):
-            text = getattr(prompts, name)
-            sm = SystemMessage(key=name,
-                               text=text)
-            lst.append(sm)
     for name in dir(medical):
         if not name.startswith('_'):
             text = getattr(medical, name)
+            sm = SystemMessage(key=name,
+                               text=text)
+            lst.append(sm)
+    for name in dir(prompts):
+        if not name.startswith('_'):
+            text = getattr(prompts, name)
             sm = SystemMessage(key=name,
                                text=text)
             lst.append(sm)
