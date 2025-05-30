62e91df14d4517c71c82cc2b90f2855e2d2379a1
Noe Javet <np.javet@gmail.com>
2025-05-30 11:03:23 +0200

    endpoints

--- frontend/src/js/endpoints.js
+++ frontend/src/js/endpoints.js
@@ -1,9 +1,10 @@
 export const endpoints = {
-    servers: '/api/servers',
-    models: '/api/models',
+    servers: '/api/ollama-servers',
+    models: '/api/ollama-models',
     systemMessages: '/api/system-messages',
     libs: '/api/libs',
     chat: '/api/chat',
+    chatDoc: '/api/chat/doc',
     runs: '/api/runs',
     resultFiles: '/api/results',
     scores: '/api/scores',
