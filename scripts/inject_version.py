0cbfaf8cfc6fa116967c63d6d33ac264a185c0bd
Noe Javet <np.javet@gmail.com>
2026-05-17 01:34:49 +0200

    mypy

--- scripts/inject_version.py
+++ scripts/inject_version.py
@@ -1,23 +1,24 @@
 import json
+
 import tomli
 
 
 def update_package_json_version():
     new_version = get_poetry_version()
-    with open('frontend/package.json') as f:
+    with open("frontend/package.json") as f:
         package = json.load(f)
-    package['version'] = new_version
-    with open('frontend/package.json', 'w') as f:
+    package["version"] = new_version
+    with open("frontend/package.json", "w") as f:
         json.dump(package, f, indent=2)
-        f.write('\n')
+        f.write("\n")
 
 
 def get_poetry_version():
-    with open('pyproject.toml', 'rb') as f:
+    with open("pyproject.toml", "rb") as f:
         data = tomli.load(f)
-    version = data.get('project')['version']
+    version = data.get("project")["version"]
     return version
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     update_package_json_version()
